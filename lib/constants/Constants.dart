12695a0eb1c1f8cdfe6fa1ff784fd4b026fb19aa
vishnu kumar <kumarvishnu1619@gmail.com>
2024-12-29 13:10:22 +0530

    editor support added

--- lib/constants/Constants.dart
+++ lib/constants/Constants.dart
@@ -1,5 +1,6 @@
 class Constants{
   static const String codeSproutBaseUrl="https://code-sprout-api-1.onrender.com/api/v1";
-  static const String compilersBaseUrl="http://192.168.212.148:9999/api/v1";
+  static const String compilersBaseUrl="https://compilers-qzxc.onrender.com/api/v1";
+  // static const String compilersBaseUrl="http://192.168.212.148:9999/api/v1";
   // static const String baseUrl="http://192.168.116.148:9093/api/v1";
 }
